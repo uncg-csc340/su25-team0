b68b3311ab7a0b4323c92de3b1a5a87354a5f155
uncg-csc340 <csc340sp24@gmail.com>
2025-05-27 20:36:38 -0400

    Added backend skeleton

---
+++ backend-api/src/main/java/com/csc340/class_connect/ClassConnectApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.class_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassConnectApplication.class, args);
+	}
+
+}
